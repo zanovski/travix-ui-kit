5ea70e95f2b36bc33bc07d65ce8780c10049ac4d
yurist38 <yurist38@mail.ru>
2018-01-15 09:35:34 +0100

    Add comment

--- styleguide.config.js
+++ styleguide.config.js
@@ -9,6 +9,9 @@ module.exports = {
       rules: [
         {
           test: /\.js?$/,
+          // we need to transpile `buble` (react-styleguide dependency) package also
+          // because it contains ES6 syntax which breaks the website on IE11.
+          // https://github.com/styleguidist/react-styleguidist/issues/764
           exclude: /node_modules\/(?!buble)/,
           use: 'babel-loader',
         },
